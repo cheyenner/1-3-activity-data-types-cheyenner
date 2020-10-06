cb2331c515703f01ab8bfacb0f1dd79580b80e23
Joe Mazzone <jmazzone@daviestech.org>
2020-10-06 11:00:37 -0400

    Initial commit

---
+++ Question2.java
@@ -0,0 +1,9 @@
+
+
+class Question2 {
+    public static void main(String[] args) {
+
+        /* Write your code here */
+
+    }
+}
---
+++ Question3.java
@@ -0,0 +1,9 @@
+
+
+class Question3 {
+    public static void main(String[] args) {
+
+        /* Write your code here */
+
+    }
+}
